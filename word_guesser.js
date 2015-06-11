0503fb11c59e0a70427284856252ad17260c7001
Allison  Craig <allison@craig.net>
2015-06-10 21:53:45 -0700

    add jQuery

--- word_guesser.js
+++ word_guesser.js
@@ -2,10 +2,12 @@ var word = ['F', 'O', 'X'];
 var goodWord = ['-', '-', '-'];
 var counter = 0;
 
-var que = window.prompt('What is your Guess?');
+var que = function(){
+    return prompt('What is your Guess?');
+}
 
-function guessLetter(guess) {
-    while (counter < word.length - 1) {
+var guessLetter = function(guess) {
+    while (counter < word.length -1) {
         var goOn = false;
         for (var x = 0; x < word.length; x++) {
             if (guess == word[x]) {
@@ -20,14 +22,27 @@ function guessLetter(guess) {
             guessLetter(yay);
         }
         if (goOn === false) {
+            debugger
             window.alert(goodWord);
             var darn = window.prompt("Darn! Maybe next time. Guess again!");
             guessLetter(darn);
         }
-        if (counter == word.length - 1) {
+        if (counter == word.length) {
+            counter += 1;
             window.alert("Yay! You got it! Nice Job!");
         }
     }
 }
 
-guessLetter(que);
+// guessLetter(que);
+
+$( document ).ready(function() {
+    console.log("this is the right one")
+
+    $('.level1').on("click", function(){
+        var awesome = que();
+        console.log(awesome)
+        guessLetter(awesome)
+
+    });
+});
